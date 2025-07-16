d2669442849cd18943da3ff7ff3f08bbcadb66c5
Steve Wang <steve.john.wang@gmail.com>
2025-07-16 19:58:17 -0400

    Add joke main class.

--- src/sumatra/main.cc
+++ src/sumatra/main.cc
@@ -1,31 +1,5 @@
-import sumatra;
-import System.out.println;
+#include "ExampleMain.h"
 
 int main() {
-  String s = "hello world";
-  String s2 = "hello world";
-  char data[] = {'a', 'b', 'c'};
-  String t = new String(data);
-  String u = new String();
-  String v;
-  Object o;
-  Object o2 = new String("abc");
-  // "hello world"
-  System.out.println(s);
-  // "abc"
-  System.out.println(t);
-  // ""
-  System.out.println(u);
-  // null
-  System.out.println(o);
-  // PKW6Object6Object#<some hex digits>
-  System.out.println(o2);
-  // true
-  System.out.println(v == null);
-  // true
-  System.out.println(s.equals(s2));
-  // false
-  System.out.println(s == s2);
-  // false
-  System.out.println(s.equals(t));
+  ExampleMain::main({});
 }
